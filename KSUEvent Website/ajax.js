ef6dc0ba3bb484b87455ec4320125498e0fac9d2
Skyl3r <kpereksta@gmail.com>
2017-10-07 09:47:37 -0400

    Ajax Function

--- KSUEvent Website/ajax.js
+++ KSUEvent Website/ajax.js
@@ -1,9 +1,26 @@
 function getOptimizedTrip() {
 	var startLocation = $("#start").val();
 	var destinationLocation = $("#finish").val();
-	var rangeStart = $("#rangeStart").val();
+	var rangeDate = $("#date");
+	var rangeStart = $("#startTimeRange").val();
 	var rangeEnd = $("#rangeEnd").val();
 	
-	$.post(
-		'website', 
+	$.get(
+		'http://kenthackapi.azurewebsites.net/api/Direction', 
 		{
+			'startingAddress': startLocation,
+			'destinationAddress': destinationLocation,
+			'startDate': rangeStart,
+			'endDate': rangeEnd,
+			'timeEnum': 0
+		},
+		function(returnedData) {
+			console.log(returnedData);
+		},
+		'json'
+	)
+}
+
+$("#submit").click(function() {
+	getOptimizedTrip();
+});
