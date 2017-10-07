9075f4a4b97713a430bdaf18edb13a04fadd5614
Skyl3r <kpereksta@gmail.com>
2017-10-07 11:53:11 -0400

    Added loading screen

--- KSUEvent Website/ajax.js
+++ KSUEvent Website/ajax.js
@@ -11,7 +11,14 @@ function getOptimizedTrip() {
 	var endDate = rangeDate + " " + rangeEnd;
 	
 	console.log(startDate);
-	
+	var options = {
+		theme:"sk-cube-grid",
+		message:'Ending world hunger... Please wait...',
+		backgroundColor:"#1847B1",
+		textColor:"white"
+	};
+
+	HoldOn.open(options);
 	$.getJSON({
 		'url': 'http://kenthackapi.azurewebsites.net/api/Direction', 
 		'data': {
@@ -23,6 +30,7 @@ function getOptimizedTrip() {
 		},
 		'success': function(returnedData) {
 			console.log(returnedData);
+			HoldOn.close();
 		},
 		'dataType': "json",
 		'headers': "Access-Control-Allow-Origin: *"
