0d3b27254662ec85ae74d67ac39de22a00290c99
Skyl3r <kpereksta@gmail.com>
2017-10-07 18:09:47 -0400

    Fixed stuff and shit

--- KSUEvent Website/ajax.js
+++ KSUEvent Website/ajax.js
@@ -1,6 +1,8 @@
 
-
+var globalVar;
 function getOptimizedTrip() {
+	
+	
 	var startLocation = $("#start").val();
 	var destinationLocation = $("#finish").val();
 	var rangeDate = $("#date").val();
@@ -10,7 +12,10 @@ function getOptimizedTrip() {
 	var startDate = rangeDate + " " + rangeStart;
 	var endDate = rangeDate + " " + rangeEnd;
 	
-	console.log(startDate);
+	
+	firstTime = moment(startDate, "MM/DD/YYYY h:mmA");
+	lastTime = moment(endDate, "MM/DD/YYYY h:mmA");
+	
 	var options = {
 		theme:"sk-cube-grid",
 		message:'Ending world hunger... Please wait...',
@@ -24,13 +29,42 @@ function getOptimizedTrip() {
 		'data': {
 			'startingAddress': startLocation,
 			'destinationAddress': destinationLocation,
-			'startDate': startDate,
-			'endDate': endDate,
-			'timeEnum': 0
+			'startDate': firstTime.format("MM/DD/YYYY HH:mm Z"),
+			'endDate': lastTime.format("MM/DD/YYYY HH:mm Z"),
+			'timeEnum': 1
 		},
 		'success': function(returnedData) {
 			console.log(returnedData);
 			HoldOn.close();
+			$("#besttraveltime").text("Below you can find the best travel times between " + $("#startTimeRange").val() + " and " + $("#endTimeRange").val());
+			$(".newEntry").remove();
+			$.each(returnedData, function(k, v) {
+				
+				var durationValues = v['duration'].split(':');
+				var hours = parseInt(durationValues[0]);
+				var minutes = parseInt(durationValues[1]);
+				
+				var titleText = ""
+				if(hours == 1)
+					titleText += "1 Hour";
+				else if(hours > 0)
+					titleText += hours.toString() + " Hours";
+				
+				if(hours > 0 && minutes > 0)
+					titleText += " and ";
+				
+				if(minutes == 1)
+					titleText += "1 Minute!";
+				else if(minutes > 0)
+					titleText += minutes.toString() + " Minutes!";
+				
+				var date = moment.utc(v['departureTime'], 'YYYY-MM-DDTHH:mm:ss');
+				//2017-10-11T05:07:30
+				
+				$("#directions-results").append("<h4 class=\"newEntry\"><strong>" + titleText + "</strong> via " + v['summary'] + " at " + date.local().format("h:mmA") + "</h4>");
+				
+			});
+			$("#directions-results").show("slow");
 		},
 		'error': function() {
 			HoldOn.close();
@@ -48,5 +82,36 @@ function getOptimizedTrip() {
 }
 
 $("#submit").click(function() {
+	$("#directions-results").hide("slow");
 	getOptimizedTrip();
 });
+
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	var strTime = hours + ':' + minutes + ' ' + ampm;
+	return strTime;
+}
+
+function timeToDate(time) {
+	var formatted = time
+	var amOrPm = 1;
+	if(time.includes("am"))
+		amOrPm = 0;
+	formatted = formatted.replace("am", "");
+	formatted = formatted.replace("pm", "");
+	
+	components = formatted.split(":");
+	
+	someDate = new Date();
+	someDate.setHours(parseInt(components[0]) + amOrPm*12);
+	someDate.setMinutes(components[1]);
+	console.log(components);
+	console.log(time)
+	console.log(someDate);
+	return someDate;
+}
